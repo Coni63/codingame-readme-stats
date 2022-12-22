58acf1e79e1890cae394694686590a8def0308de
nicolas <mine.nicolas@gmail.com>
2022-12-22 21:44:02 +0100

    try loading svg in the svg itself

--- api/application/svg_builder.py
+++ api/application/svg_builder.py
@@ -1,12 +1,21 @@
 import math
 
 import cairo
-from io import BytesIO
+from io import BytesIO, StringIO
 
 from config import constants
+import base64 
+import svgutils
+from lxml import etree
+
+def decode_svg(svg_encoded):
+    encoded = svg_encoded.replace("data:image/svg+xml;base64,", "")
+    return base64.b64decode(encoded).decode("utf-8") 
+
 
 def render(data):
     f = BytesIO()
+    f2 = BytesIO()
 
     with cairo.SVGSurface(f, constants.SVG_width, constants.SVG_height) as surface:
         # creating a cairo context object
@@ -28,14 +37,34 @@ def render(data):
         # context.set_line_cap(cairo.LINE_CAP_SQUARE)
         context.arc(50, 50, 30, 3*math.pi/2, 6.7*math.pi/2)  # cx, cy, radius, angle_start, angle_end
         context.stroke()
+
+        context.set_source_rgba(1, 0, 0, 1)
+        context.rectangle(50, 50, 29, 29)  # x, y, width, height
+        context.fill()
     
         # getting all the svg versions available
         versions = surface.get_versions()
 
-    surface = cairo.ImageSurface.create_for_data(data, cairo.FORMAT_ARGB32, width, height)
-    ctx = cairo.Context(surface)
+    a = etree.XML(f.getvalue())
 
-    return f.getvalue()
+    SVG_collaboration = etree.fromstring(decode_svg(constants.SVG_collaboration))
+    SVG_algorithmes = etree.fromstring(decode_svg(constants.SVG_algorithmes))
+    SVG_optimization = etree.fromstring(decode_svg(constants.SVG_optimization))
+    SVG_speed = etree.fromstring(decode_svg(constants.SVG_speed))
+    SVG_AI = etree.fromstring(decode_svg(constants.SVG_AI))
+
+    d = svgutils.transform.SVG(decode_svg(constants.SVG_AI))
+    d.moveto(50, 50)
+
+    c = svgutils.transform.fromstring(decode_svg(constants.SVG_AI))
+    # c.moveto(50, 50)
+
+
+    fig = svgutils.transform.SVGFigure("500px", "200px")
+    fig.append([a, d])
+
+    return fig.to_str()
+    # return svgutils.compose.Figure("500px", "200px", *all_trees).tostr()
 
 
 # https://www.geeksforgeeks.org/pycairo-drawing-different-type-of-line-caps/?ref=rp
