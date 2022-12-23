92891766a2f1bb51532e4574c9ad26aafea748dd
nicolas <mine.nicolas@gmail.com>
2022-12-23 17:28:06 +0100

    improved layout

--- api/application/svg_builder.py
+++ api/application/svg_builder.py
@@ -33,8 +33,58 @@ def get_square(context, nth_position, bg_color):
     roundrect(context, *get_position(nth_position), s, s, 5)
     context.fill()
 
+def get_certif_label(context, nth_position, text, bg_color):
+    position = get_position(nth_position)
+    color = hex_to_rgb(bg_color)
+    context.set_source_rgb(*color)
+    context.select_font_face("Purisa", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+    context.set_font_size(13)
+    context.move_to(190, position[1]+15)
+    context.show_text(text)
+    context.stroke()
+
+def get_stats_label(context, nth_position, text, bg_color):
+    position = get_position(nth_position)
+    color = hex_to_rgb(bg_color)
+    context.set_source_rgb(*color)
+    context.select_font_face("Purisa", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+    context.set_font_size(13)
+    context.move_to(390, position[1]+15)
+    context.show_text(text)
+    context.stroke()
+
+def draw_line(context, x1, y1, x2, y2, color):
+    context.set_source_rgb(*hex_to_rgb(color))
+    context.set_line_width(5)
+    context.move_to(x1, y1)
+    context.line_to(x2, y2) 
+    context.stroke()
+
+def draw_borders(context, color):
+    context.set_source_rgb(*hex_to_rgb(color))
+    context.set_line_width(5)
+    # context.rectangle(0, 0, constants.SVG_width, constants.SVG_height)  # x, y, width, height
+    roundrect(context, 0, 0, constants.SVG_width, constants.SVG_height, 20)
+    context.stroke()
+
+def get_title(context, text, font_color):
+    color = hex_to_rgb(font_color)
+    context.set_source_rgb(*color)
+    context.select_font_face("Purisa", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+    context.set_font_size(35)
+    context.move_to(20, 35)
+    context.show_text(text)
+    xbearing, ybearing, width, height, xadvance, yadvance = context.text_extents(text)
+
+    context.select_font_face("Purisa", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+    context.set_font_size(25)
+    context.move_to(20 + width + 2, 35)
+    context.show_text("'s profile")
+
+    context.stroke()
+
 def get_position(n = 1):
-    return (160, n * 25 - 20)
+    return (160, n * 25 + 20)
 
 def get_scale():
     return 0.66
@@ -46,57 +96,98 @@ def hex_to_rgb(value):
     b = value[4:]
     return (int(r, 16)/255.0, int(g, 16)/255.0, int(b, 16)/255.0)
 
+def get_rank(score):
+    return "A+"
+
+def create_pie(context, score, font_color, background_color):
+    start_angle = 3*math.pi/2
+    end_angle = 3*math.pi/2 + 2*math.pi*score/100
+    center = (80, 105)
+    radius = 50
+
+    context.set_source_rgb(*hex_to_rgb(background_color))
+    context.set_line_width(10)
+    context.set_line_cap(cairo.LINE_CAP_BUTT)
+    context.arc(*center, radius,  end_angle, start_angle)  # cx, cy, radius, angle_start, angle_end
+    context.stroke()
+
+    context.set_source_rgb(*hex_to_rgb(font_color))
+    context.set_line_width(12)
+    context.set_line_cap(cairo.LINE_CAP_ROUND)
+    context.arc(*center, radius, start_angle, end_angle)  # cx, cy, radius, angle_start, angle_end
+    context.stroke()
+
+    context.set_source_rgb(*hex_to_rgb(font_color))
+        
+    context.select_font_face("Purisa", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+    context.set_font_size(35)
+    context.move_to(center[0]-18, center[1]+10)
+    context.show_text(get_rank(score))
+    context.stroke()
+
 def render(data):
     f = BytesIO()
 
     with cairo.SVGSurface(f, constants.SVG_width, constants.SVG_height) as surface:
         # creating a cairo context object
         context = cairo.Context(surface)
-    
-        context.set_source_rgba(0, 0, 0, 1)
-        context.rectangle(0, 0, constants.SVG_width-10, constants.SVG_height-10)  # x, y, width, height
-        context.stroke()
 
-        context.set_source_rgba(1, 0, 0, 1)
-        context.set_line_width(10)
-        context.set_line_cap(cairo.LINE_CAP_BUTT)
-        context.arc(50, 50, 30,  6.7*math.pi/2, 3*math.pi/2)  # cx, cy, radius, angle_start, angle_end
-        context.stroke()
 
-        context.set_source_rgba(0, 0, 0, 1)
-        context.set_line_width(12)
-        context.set_line_cap(cairo.LINE_CAP_ROUND)
-        # context.set_line_cap(cairo.LINE_CAP_SQUARE)
-        context.arc(50, 50, 30, 3*math.pi/2, 6.7*math.pi/2)  # cx, cy, radius, angle_start, angle_end
-        context.stroke()
+        draw_borders(context, constants.COLOR_LEGEND)
 
-        # SQUARE BACKGROUND FOR IMG
-        get_square(context, 1, "#FF0000")
-        get_square(context, 2, "#00FF00")
-        get_square(context, 3, "#FF00FF")
-        get_square(context, 4, "#FFFF00")
-        get_square(context, 5, "#00FFFF")
+        # pie with score & note
+        create_pie(context, 65, font_color=constants.COLOR_LEGEND, background_color=constants.COLOR_SILVER)
+
+        get_title(context, "username", font_color=constants.COLOR_LEGEND)
+
+        # SQUARE BACKGROUND FOR CERTIFS
+        get_square(context, 1.5, constants.COLOR_LEGEND)
+        get_square(context, 2.5, constants.COLOR_GOLD)
+        get_square(context, 3.5, constants.COLOR_SILVER)
+        get_square(context, 4.5, constants.COLOR_BRONZE)
+        get_square(context, 5.5, constants.COLOR_LEGEND)
+
+        # LABEL CERTIFS
+        s = "{title}:{ranking}"
+        # get_certif_label(context, 1, "CERTIFICATIONS", constants.COLOR_LEGEND)
+        get_certif_label(context, 1.5, s.format(title="Collaboration", ranking="LEGEND"), constants.COLOR_LEGEND)
+        get_certif_label(context, 2.5, s.format(title="Algorithmes", ranking="GOLD"), constants.COLOR_GOLD)
+        get_certif_label(context, 3.5, s.format(title="Optimization", ranking="SILVER"), constants.COLOR_SILVER)
+        get_certif_label(context, 4.5, s.format(title="Coding Speed", ranking="BRONZE"), constants.COLOR_BRONZE)
+        get_certif_label(context, 5.5, s.format(title="AI", ranking="LEGEND"), constants.COLOR_LEGEND)
+
+        # DRAW SEPARATORS
+        draw_line(context, 150, 50, 150, 190, constants.COLOR_LEGEND)
+        draw_line(context, 360, 50, 360, 190, constants.COLOR_LEGEND)
+
+        # LABEL STATS
+        get_stats_label(context, 1, s.format(title="Global Rank", ranking="1234/654321"), constants.COLOR_LEGEND)
+        get_stats_label(context, 2, s.format(title="Puzzle Solved", ranking="530"), constants.COLOR_GOLD)
+        get_stats_label(context, 3, s.format(title="Level", ranking="38"), constants.COLOR_SILVER)
+        get_stats_label(context, 4, s.format(title="Success", ranking="350/500"), constants.COLOR_BRONZE)
+        get_stats_label(context, 5, s.format(title="Best Language", ranking="Python"), constants.COLOR_LEGEND)
+        get_stats_label(context, 6, s.format(title="Highest Compet.", ranking="1200/15000"), constants.COLOR_LEGEND)
+
 
         # getting all the svg versions available
         versions = surface.get_versions()
 
-    a = etree.XML(f.getvalue())
+    # the card is the component having nearly everything except external icons from CG
+    card = etree.XML(f.getvalue())
 
-    SVG_collaboration = place_icon(constants.SVG_collaboration, nth_position=1)
-    SVG_algorithmes = place_icon(constants.SVG_algorithmes, nth_position=2)
-    SVG_optimization = place_icon(constants.SVG_optimization, nth_position=3)
-    SVG_speed = place_icon(constants.SVG_speed, nth_position=4)
-    SVG_AI = place_icon(constants.SVG_AI, nth_position=5)
+    # load icons from constants (extracted from CG)
+    SVG_collaboration = place_icon(constants.SVG_collaboration, nth_position=1.5)
+    SVG_algorithmes = place_icon(constants.SVG_algorithmes, nth_position=2.5)
+    SVG_optimization = place_icon(constants.SVG_optimization, nth_position=3.5)
+    SVG_speed = place_icon(constants.SVG_speed, nth_position=4.5)
+    SVG_AI = place_icon(constants.SVG_AI, nth_position=5.5)
 
-
+    # merge all svg in one
     fig = svgutils.transform.SVGFigure()
     fig.set_size(("{constants.SVG_width}px", "{constants.SVG_height}px"))
-    fig.append([a, SVG_collaboration, SVG_algorithmes, SVG_optimization, SVG_speed, SVG_AI])
+    fig.append([card, SVG_collaboration, SVG_algorithmes, SVG_optimization, SVG_speed, SVG_AI])
     return fig.to_str()
-    # return svgutils.compose.Figure("500px", "200px", *all_trees).tostr()
 
 
 # https://www.geeksforgeeks.org/pycairo-drawing-different-type-of-line-caps/?ref=rp
 # https://pycairo.readthedocs.io/en/latest/reference/enums.html
-
-# <svg xmlns="http://www.w3.org/2000/svg" width="495" height="195" viewBox="0 0 495 195" fill="none"><script xmlns=""/><link xmlns="" type="text/css" rel="stylesheet" id="dark-mode-custom-link"/><link xmlns="" type="text/css" rel="stylesheet" id="dark-mode-general-link"/><style xmlns="" lang="en" type="text/css" id="dark-mode-custom-style"/><style xmlns="" lang="en" type="text/css" id="dark-mode-native-style"/><style xmlns="" lang="en" type="text/css" id="dark-mode-native-sheet"/><script xmlns="" id="youtube-hd-fjdmkanbdloodhegphphhklnjfngoffa">var ythdlog = () =&gt; {};;var ythderror = () =&gt; {};</script>
