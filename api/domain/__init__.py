1fc94e6cc0d4b1ee7270a7fa4e82c4e85c2e7d3f
nicolas <mine.nicolas@gmail.com>
2023-01-20 23:23:24 +0100

    remove ranking history to reduce storage

--- api/domain/__init__.py
+++ api/domain/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
-from domain.interfaces.i_user_info import IUserDto
+from domain.interfaces.i_user_info import IUserDto, IHistoricsDto
 from domain.interfaces.i_language import ILanguageDto
 from domain.interfaces.i_certification import ICertificationDto
 from domain.interfaces.i_achievement import IAchievementDto
